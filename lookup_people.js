96d697e5b19d96f2d5c26794b00c3da3c5039bfc
cecstar <yocecia@gmail.com>
2016-10-26 00:57:17 +0000

    Lookup Script function complete, takes arg from process.argv. Have not refactored.

---
+++ lookup_people.js
@@ -0,0 +1,26 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const inputs = process.argv[2];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE first_name=$1 OR last_name = $1;`, [inputs], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    var results = result.rows[0];
+    console.log(`Found 1 person(s) by the name: '${inputs}': - ${results.id}: ${results.first_name} ${results.last_name}, born '${results.birthdate.toDateString()}'`);
+    client.end();
+  });
+});
